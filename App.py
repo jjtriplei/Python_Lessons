003d3d7d93d7ae431cb2e92397c5a0f2900539d8
jjtriplei <jjtriplei@gmail.com>
2015-03-15 13:02:17 -0700

    Updated Index

--- App.py
+++ App.py
@@ -2,14 +2,13 @@ from flask import Flask
 from flask import render_template
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Josh is a douche! That should offend less people'
+#@app.route('/')
+#def hello_world():
+#    return 'Josh is a douche! That should offend less people'
 
-@app.route('/Craft/') # 
-def Cool_shit():
+@app.route('/') # 
+def Index():
     return render_template('Craft.html')
-
 
 
 if __name__ == '__main__':
