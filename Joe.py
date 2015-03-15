7d69112298b8ab5879510a63096c088290accc37
jjtriplei <jjtriplei@gmail.com>
2015-03-15 12:43:40 -0700

    Initial Commit

---
+++ Joe.py
@@ -0,0 +1,16 @@
+
+
+#print "Joe is the SHIT"
+
+#Cartman: On Monday she's a bitch, 
+#on Tuesday she's a bitch, 
+#on Wensday though Saturday she's a bitch, 
+#then on Sunday just to be different she's a super King Kamehameha beeeotch!
+
+
+
+
+Bitch = open('BitchJosh.txt', "r")
+
+print Bitch.read()
+
